28a307fd1a5b08466a0de78abc7465958b5310e7
Ivann <212ivann.rambaud@gmail.com>
2024-05-07 11:53:35 +0200

    création des pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeArtisanComponent } from './liste-artisan/liste-artisan.component';
+import { FicheArtisanComponent } from './fiche-artisan/fiche-artisan.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccueilComponent,
+    ListeArtisanComponent,
+    FicheArtisanComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
