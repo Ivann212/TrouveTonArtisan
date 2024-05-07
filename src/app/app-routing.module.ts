210528d107b8141f720d1d53698e1f7afbc8c46d
Ivann <212ivann.rambaud@gmail.com>
2024-05-07 15:38:22 +0200

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
 
-const routes: Routes = [];
+import { AccueilComponent } from './accueil/accueil.component';
+import { FicheArtisanComponent } from './fiche-artisan/fiche-artisan.component';
+import { ListeArtisanComponent } from './liste-artisan/liste-artisan.component';
+import { Page404Component } from './page404/page404.component';
+
+const routes: Routes = [
+  {path: "", component: AccueilComponent}, 
+  {path: "ficheArtisan", component: FicheArtisanComponent}, 
+  {path: "listeArtisan", component: ListeArtisanComponent}, 
+  {path: "**", pathMatch:'full', component: Page404Component} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
